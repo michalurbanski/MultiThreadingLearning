3efb69471870aa1527ff560ff1e7d69660f66c30
Michal Urbanski <michal_urbanski@windowslive.com>
2016-06-11 15:13:49 +0000

    Delegates refactoring

--- ThreadsApp/Program.cs
+++ ThreadsApp/Program.cs
@@ -11,18 +11,25 @@ namespace ThreadsApp
     class Program
     {
         public delegate int BinaryOp(int x, int y);
+        private static Action action; 
 
         static void Main(string[] args)
         {
             //GetBasicThreadInfo(); 
 
+            action = DelegatesExample;
+
+            ExecuteAction(action);
+        }
+
+        private static void ExecuteAction(Action action)
+        {
             Console.WriteLine("Main() thread invoked on {0}", Thread.CurrentThread.ManagedThreadId);
 
-            BinaryOp operation = new BinaryOp(Add);
-            int answer = operation(10, 5);
+            action();
 
             Console.WriteLine("Doing more work in main thread");
-            Console.ReadLine(); 
+            Console.ReadLine();
         }
 
         private static void GetBasicThreadInfo()
@@ -30,6 +37,16 @@ namespace ThreadsApp
             Thread thread = Thread.CurrentThread;
             AppDomain domain = Thread.GetDomain();
             Context context = Thread.CurrentContext;
+        }
+
+        /// <summary>
+        /// Using this delegates approach main thread is blocked. Until Add action is finished main 
+        /// thread is awaiting. 
+        /// </summary>
+        private static void DelegatesExample()
+        {
+            BinaryOp operation = new BinaryOp(Add);
+            int answer = operation(10, 5);
         }
 
         private static int Add(int x, int y)
